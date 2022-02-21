7dbee4299c5d11cb8f2e65ca6d85f8ca7298acb6
Hoa Le <Hoa_le9485@yahoo.com>
2022-02-21 02:27:35 -0500

    done

--- react-frontend/src/index.js
+++ react-frontend/src/index.js
@@ -5,7 +5,7 @@ import "./index.css";
 import {
   BrowserRouter as Router, Route
 } from "react-router-dom";
-
+const PORT = process.env.PORT || 5000
 ReactDOM.render(
   <Router>
     <Route path="/" component={App}/>
